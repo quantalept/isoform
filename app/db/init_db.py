c4d7bc174f249d2776a206de4f71ecbee9c416d4
Aloysus <11aloysh2001@gmail.com>
2025-05-16 15:53:28 +0530

    Revert "Initial commit with schema n models"
    This reverts commit 76e6c7d3a42ed602ffaaf567ae5077aa3641711a.

--- app/db/init_db.py
+++
@@ -1,6 +0,0 @@
-from db.session import engine
-from db.base import Base
-from models.user import User  # Make sure model is imported
-
-def init_db():
-    Base.metadata.create_all(bind=engine)
--- app/main.py
+++
@@ -1,8 +0,0 @@
-from fastapi import FastAPI
-from app.api import user,product,purchase
-
-app = FastAPI(title="FastAPI Template")
-
-app.include_router(user.router, prefix="/api/v1", tags=["Users"])
-app.include_router(product.router, prefix="/api/v1", tags=["Products"])
-app.include_router(purchase.router, prefix="/api/v1", tags=["Purchases"])
